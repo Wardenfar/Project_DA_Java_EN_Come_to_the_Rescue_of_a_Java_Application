76676748c5de2b39a4e707797e38aa2aaa7c36a9
Wardenfar <theo.emeriau@gmail.com>
2020-10-31 15:11:18 +0100

    corrections and comments
    change AnalyticsCounter
    add ISymptomProcessor + add AnalyticsCounterProcessor
    change ISymptomReader + moved ReadSymptomDataFromFile

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,78 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import com.hemebiotech.analytics.processor.AnalyticsCounterProcessor;
+import com.hemebiotech.analytics.reader.ReadSymptomDataFromFile;
+
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    /**
+     * Main function of Analytics Program who
+     * <p>
+     * read the file "symptoms.txt",
+     * process them with {@link AnalyticsCounterProcessor},
+     * then write the counters to "result.out" using {@link AnalyticsCounter#writeCountersInAlphabeticOrder(String, Map)}
+     *
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        // Read symptoms from file : symptoms.txt
+        ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+        List<String> symptoms = reader.getSymptoms();
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+        // Process symptoms in an Analytics Counter Processor
+        AnalyticsCounterProcessor processor = new AnalyticsCounterProcessor();
+        processor.processSymptoms(symptoms);
+
+        // Retrieve counters
+        Map<String, Integer> counters = processor.getCounters();
+
+        // Write counters to : result.out
+        writeCountersInAlphabeticOrder("result.out", counters);
+    }
+
+    /**
+     * Write counters in the file specified, one line per counter with the format :
+     * <p>
+     * [symptom]: [count]
+     * [symptom]: [count]
+     * ...
+     *
+     * @param filePath full or relative path to file to write counters
+     * @param counters counters from {@link AnalyticsCounterProcessor#getCounters()}
+     */
+    public static void writeCountersInAlphabeticOrder(String filePath, Map<String, Integer> counters) {
+        try {
+            // Open file to write analytics
+            FileWriter writer = new FileWriter(filePath);
+
+            // Get keys (symptoms) of counters (no duplicates)
+            List<String> symptoms = new ArrayList<>(counters.keySet());
+
+            // Sort in Alphabetic Order
+            symptoms.sort(String::compareTo);
+
+            // Foreach symptom
+            for (String symptom : symptoms) {
+                int count = counters.get(symptom);
+
+                // Write line with format: "[symptom]: [count]\n"
+                writer.write(symptom);
+                writer.write(": ");
+                writer.write(String.valueOf(count));
+                writer.write("\n");
+            }
+
+            // Close writer
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("Error while writing counters to file : " + filePath);
+            e.printStackTrace();
+        }
+    }
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/processor/AnalyticsCounterProcessor.java
@@ -0,0 +1,56 @@
+package com.hemebiotech.analytics.processor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Process list of symptoms to analyse number of occurrences of each symptom
+ */
+public class AnalyticsCounterProcessor implements ISymptomProcessor {
+
+    /**
+     * @see Map
+     * - key = symptom
+     * - value = number of occurrences
+     */
+    private Map<String, Integer> counters;
+
+    /**
+     * Constructor of {@link AnalyticsCounterProcessor}
+     */
+    public AnalyticsCounterProcessor() {
+        counters = new HashMap<>(); // initialize counters to empty map
+    }
+
+    /**
+     * Implementation of {@link ISymptomProcessor#processSymptoms(List)}
+     * <p>
+     * Foreach symptom :
+     * if it's a new symptom then create new counter
+     * increment counter by 1
+     *
+     * @param symptoms list of symptoms (may contain many duplications)
+     */
+    @Override
+    public void processSymptoms(List<String> symptoms) {
+        for (String symptom : symptoms) { // Foreach symptoms
+            // If it's new symptom then create new counter
+            if (!getCounters().containsKey(symptom)) {
+                getCounters().put(symptom, 0);
+            }
+            // get previous count and add 1
+            int previousCount = getCounters().get(symptom);
+            getCounters().put(symptom, previousCount + 1);
+        }
+    }
+
+    /**
+     * Getter of {@link AnalyticsCounterProcessor#counters}
+     *
+     * @return return counters variable
+     */
+    public Map<String, Integer> getCounters() {
+        return counters;
+    }
+}
