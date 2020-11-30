5480b78b48a93ae39ad27ea58ca570070df7790c
Theo EMERIAU <theo.emeriau@gmail.com>
2020-11-30 09:03:03 +0100

    separate function "writeCountersInAlphabeticOrder" + change main to instantiate AnalyticsCounter class

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -6,23 +6,43 @@ import com.hemebiotech.analytics.reader.ReadSymptomDataFromFile;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class AnalyticsCounter {
 
+
     /**
-     * Main function of Analytics Program who
-     * <p>
-     * read the file "symptoms.txt",
-     * process them with {@link AnalyticsCounterProcessor},
-     * then write the counters to "result.out" using {@link AnalyticsCounter#writeCountersInAlphabeticOrder(String, Map)}
+     * Main function of Analytics Program that create an {@link AnalyticsCounter}
+     * and call {@link AnalyticsCounter#process(String, String)}
      *
      * @param args not used
      */
     public static void main(String[] args) {
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+        analyticsCounter.process("symptoms.txt", "result.out");
+    }
+
+    /**
+     * Default Constructor of {@link AnalyticsCounter}
+     */
+    public AnalyticsCounter() {
+
+    }
+
+    /**
+     * Process function of Analytics Counter who
+     * <p>
+     * read the file "symptoms.txt",
+     * process them with {@link AnalyticsCounterProcessor},
+     * then write the counters to "result.out" using {@link AnalyticsCounter#writeCounters(String, List, Map)} )}
+     *
+     * @param inputFilePath not used
+     */
+    public void process(String inputFilePath, String outputFilePath) {
         // Read symptoms from file : symptoms.txt
-        ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+        ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile(inputFilePath);
         List<String> symptoms = reader.getSymptoms();
 
         // Process symptoms in an Analytics Counter Processor
@@ -32,8 +52,20 @@ public class AnalyticsCounter {
         // Retrieve counters
         Map<String, Integer> counters = processor.getCounters();
 
+        // Sort keys of counters (symptoms)
+        List<String> symptomsSorted = sortSymptomsByAlphabeticOrder(counters.keySet());
+
         // Write counters to : result.out
-        writeCountersInAlphabeticOrder("result.out", counters);
+        writeCounters(outputFilePath, symptomsSorted, counters);
+    }
+
+    public List<String> sortSymptomsByAlphabeticOrder(Collection<String> symptoms){
+        // Clone the collection
+        List<String> sorted = new ArrayList<>(symptoms);
+
+        // Sort in Alphabetic Order
+        sorted.sort(String::compareTo);
+        return sorted;
     }
 
     /**
@@ -46,19 +78,13 @@ public class AnalyticsCounter {
      * @param filePath full or relative path to file to write counters
      * @param counters counters from {@link AnalyticsCounterProcessor#getCounters()}
      */
-    public static void writeCountersInAlphabeticOrder(String filePath, Map<String, Integer> counters) {
+    public void writeCounters(String filePath, List<String> symptomsSorted, Map<String, Integer> counters) {
         try {
             // Open file to write analytics
             FileWriter writer = new FileWriter(filePath);
 
-            // Get keys (symptoms) of counters (no duplicates)
-            List<String> symptoms = new ArrayList<>(counters.keySet());
-
-            // Sort in Alphabetic Order
-            symptoms.sort(String::compareTo);
-
             // Foreach symptom
-            for (String symptom : symptoms) {
+            for (String symptom : symptomsSorted) {
                 int count = counters.get(symptom);
 
                 // Write line with format: "[symptom]: [count]\n"
